de45f45fba089b9049e80f0267b3607ccb5283da
Hub-Mo <Mohamed.elja@hotmail.com>
2022-06-02 11:41:11 +0200

    changed the spiritual animal part

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 let box = document.getElementById('box');
 boxRect = box.getBoundingClientRect();
 let ball = document.getElementById('ball')
+ball.setAttribute('class', 'theBall');
 let rect = ball.getBoundingClientRect();
 ball.style.position = 'relative';
 ball.style.transition = '3s';
-console.log(boxRect);
-console.log(rect);
+console.log(ball);
 setInterval(function magic(){
     if ( rect.x === 9) {
         rect.x = 850;
@@ -21,6 +21,23 @@ setInterval(function magic(){
         ball.style.transform = 'rotate(0deg)'
     }
 }, 3000);
+
+let animal = document.querySelector('.column');
+animal.innerHTML = "";
+function edit() {
+    let h3 = document.createElement('h3');
+    h3.innerHTML = ('Spiritual animal')
+    let pOne = document.createElement('p');
+    pOne.innerHTML = ('lion');
+    let pTwo = document.createElement('p');
+    pTwo.innerHTML = ('bcuse lion is cool AF like Mo');
+    let img = document.createElement('img')
+    img.src = ('https://upload.chatsdumonde.com/img_global/25-le-chat/les-autres-felins/le-lion-morphologie-alimentation-mode-de-vie/_light-1290_14024-un-magnifique-lion-blanc-adulte-allonge-dans-l-herbe.jpg')
+    img.setAttribute('width', '150')
+    animal.append(h3, pOne,pTwo, img);
+}
+console.log(animal);
+edit()
 
 
 
