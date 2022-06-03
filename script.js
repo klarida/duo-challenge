dbf6197b8d2cf16184e1706fba66fd75f7e298b1
Hub-Mo <Mohamed.elja@hotmail.com>
2022-06-03 10:01:54 +0200

    finished the adding part of the challenge

--- script.js
+++ script.js
@@ -7,25 +7,57 @@ ball.setAttribute('class', 'theBall');
 let rect = ball.getBoundingClientRect();
 ball.style.position = 'relative';
 ball.style.transition = '3s';
-console.log(ball);
+ball.style.margin = '1px';
 
 
 setInterval(function magic(){
-    let righty = boxRect.right - 57;
+        let righty = boxRect.right - 57;
 
-    if ( rect.x === 9) {
-        rect.x = 850;
-        ball.style.left = righty+'px'
-        ball.style.transform = 'rotate(1000deg)'
-
-    }
-    else if (rect.x === 850){
-        console.log('jhdsjhkdhjbkdqs');
-        rect.x = 9; 
-        ball.style.left = boxRect.left+'px'
-        ball.style.transform = 'rotate(0deg)'
-    }
+        if ( rect.x === 9) {
+            rect.x = 850;
+            ball.style.left = righty+'px'
+            ball.style.transform = 'rotate(1000deg)'
+        }
+        else if (rect.x === 850){
+            rect.x = 9; 
+            ball.style.left = boxRect.left+'px'
+            ball.style.transform = 'rotate(0deg)'
+        }
 }, 3000);
+
+
+
+let cloneys = document.querySelectorAll('theBall');
+var run = 0;
+let CloneInterval = setInterval(function theMagicTwo(){
+    run += 1;
+    let clone = ball.cloneNode(true);
+    box.append(clone);
+    let rectClone = clone.getBoundingClientRect();
+    let rightyClone = boxRect.right - 57;
+
+    clone.style.position = 'relative';
+    clone.style.transition = '3s';
+
+
+
+
+    if ( rectClone.x === 18) {
+        rectClone.x = 871.9939575195312;
+        clone.style.left = rightyClone+'px'
+        clone.style.transform = 'rotate(1000deg)'
+    }
+    else if (rectClone.x === 871.9939575195312){
+        rectClone.x = 17; 
+        clone.style.left = boxRect.left+'px'
+        clone.style.transform = 'rotate(0deg)'
+    }
+    if(run == 5){
+        clearInterval(CloneInterval);
+    }
+},6000);
+
+
 
 
 // changing spiritual animal
@@ -43,7 +75,6 @@ function edit() {
     img.setAttribute('width', '150')
     animal.append(h3, pOne,pTwo, img);
 }
-console.log(animal);
 edit()
 
 
